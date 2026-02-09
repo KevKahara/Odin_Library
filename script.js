5799e4a75184b544d9067cdb44d90815f5618375
Kevin Kahara <kaharakevin254@gmail.com>
2026-02-10 02:09:47 +0300

    Javascripy logic to be added

---
+++ script.js
@@ -0,0 +1,8 @@
+// Library starts empty
+// User fills form
+// Form data → create book
+// Book → added to library
+// Library → displayed on screen
+// User clicks remove
+// Book removed from library
+// Screen rebuilt
