329056683c3a023bd55bb4f8ac29152837290c0a
Kevin Kahara <kaharakevin254@gmail.com>
2026-02-11 18:09:26 +0300

    Hookup the form element so that the user will enter book details from the form

--- script.js
+++ script.js
@@ -22,3 +22,59 @@ function toggleReadStatus(id) {
         foundBook.isRead = !foundBook.isRead;
     }
 }
+
+function renderLibrary() {
+
+    const displayBook = document.querySelector('#book-container');
+    displayBook.textContent = '';
+
+    myLibrary.forEach((book) => {
+
+        const card = document.createElement('div');
+        card.classList.add('book-card');
+
+        const title = document.createElement('p');
+        const author = document.createElement('p');
+        const pages = document.createElement('p');
+        const ReadStatus = document.createElement('p');
+
+        title.textContent = `TITLE: ${book.title}`;
+        author.textContent = `AUTHOR: ${book.author}`;
+        pages.textContent = `PAGES: ${book.pages}`;
+        ReadStatus.textContent = `READ STATUS: ${book.isRead ? 'Read' : 'Not read'}`;
+
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(ReadStatus);
+
+        displayBook.appendChild(card);
+
+        const toggleButton = document.createElement('button');
+        toggleButton.classList.add('toggle-btn');
+        toggleButton.textContent = 'Toggle Read';
+
+        toggleButton.addEventListener('click', () => {
+            toggleReadStatus(book.id);
+            renderLibrary();
+        });
+
+        card.appendChild(toggleButton);
+
+        const removeBookButton = document.createElement('button');
+        removeBookButton.classList.add('remove-btn');
+        removeBookButton.textContent = 'Remove Book';
+
+        removeBookButton.addEventListener('click', () => {
+            removeBookFromLibrary(book.id);
+            renderLibrary();
+        });
+
+        card.appendChild(removeBookButton);
+
+    });
+
+}
+
+
+renderLibrary();
