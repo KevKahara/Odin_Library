f839bd42e4810949878f0a5288ca6b68ccbc21e7
Kevin Kahara <kaharakevin254@gmail.com>
2026-02-11 08:23:23 +0300

    The data layer is now complete I will now need to work on the presentation layer of the project

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-// Library starts empty
-// User fills form
-// Form data → create book
-// Book → added to library
-// Library → displayed on screen
-// User clicks remove
-// Book removed from library
-// Screen rebuilt
+let myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function removeBookFromLibrary(id) {
+    myLibrary = myLibrary.filter(book => book.id !== id);
+}
+
+function toggleReadStatus(id) {
+    const foundBook = myLibrary.find(book => book.id === id);
+    if(foundBook) {
+        foundBook.isRead = !foundBook.isRead;
+    }
+}
